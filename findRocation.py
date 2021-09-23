5683388fdc6ed7c63dc8787b4f9b1cb093d6c8b4
Garyson <bbt1250912@gmail.com>
2021-09-23 23:08:12 +0900

    ver 1.1.1

--- findRocation.py
+++ findRocation.py
@@ -12,14 +12,29 @@ def checkNone(msgReturnValue):
         pg.alert(text='오류', title='창을 종료합니다.', button='종료')
         exit()
     return msgReturnValue
+def coordinateCalculation(x1,x2):
+    x2 = x2 -x1
+    return x2
+
+def changeInt(list):
+    for i in range(len(list)):
+        list[i]=int(list[i])
+    return list
 
 
-inputdata = pg.prompt(text="시간 좌표 입력",title="시간 인식 데이터",default="x1 y2 (x2-x1) (y2-y1)")
-inquiryInput =pg.prompt(text="조회 좌표 입력",title="조회 인식 데이터",default="x1 y2 (x2-x1) (y2-y1)")
+inputdata = pg.prompt(text="시간 좌표 입력",title="시간 인식 데이터",default="x1 y1 x2 y2")
+inquiryInput =pg.prompt(text="조회 좌표 입력",title="조회 인식 데이터",default="x1 y1 x2 y2")
 
 inquirySplit = inquiryInput.split(" ")
+inquirySplit = changeInt(inquirySplit)
+#x2-x1 and y2-y1
+inquirySplit[2] = coordinateCalculation(inquirySplit[0],inquirySplit[2])
+inquirySplit[3] = coordinateCalculation(inquirySplit[1],inquirySplit[3])
 
 inputSplit = inputdata.split(" ")
+inputSplit = changeInt(inputSplit)
+inputSplit[2] = coordinateCalculation(inputSplit[0],inputSplit[2])
+inputSplit[3] = coordinateCalculation(inputSplit[1],inputSplit[3])
 for i in range(len(inputSplit)):
     inputSplit[i] = float(inputSplit[i])
 
@@ -39,13 +54,17 @@ while True:
         checkNone(recognition)
 move_mouse(x,y)
 
-errorInput =pg.prompt(text="오류 좌표 입력",title="오류 인식 데이터",default="x1 y2 (x2-x1) (y2-y1)")
-errorSplit = errorInput.split(" ")
-pg.screenshot('./img/error.png', region=(errorSplit[0], errorSplit[1], errorSplit[2], errorSplit[3]))
+# errorInput =pg.prompt(text="오류 좌표 입력",title="오류 인식 데이터",default="x1 y1 x2 y2)")
+# errorSplit = errorInput.split(" ")
+# errorSplit[2] = coordinateCalculation(errorSplit[0],errorSplit[2])
+# errorSplit[3] = coordinateCalculation(errorSplit[1],errorSplit[3])
+# pg.screenshot('./img/error.png', region=(errorSplit[0], errorSplit[1], errorSplit[2], errorSplit[3]))
 
-confirmInput = pg.prompt(text="확인 좌표 입력",title="확인 인식 데이터",default="x1 y2 (x2-x1) (y2-y1)")
-confirmSplit = confirmInput.split(" ")
-pg.screenshot('./img/confirm.png', region=(confirmSplit[0], confirmSplit[1], confirmSplit[2], confirmSplit[3]))
+# confirmInput = pg.prompt(text="확인 좌표 입력",title="확인 인식 데이터",default="x1 y1 x2 y2)")
+# confirmSplit = confirmInput.split(" ")
+# confirmSplit[2] = coordinateCalculation(confirmSplit[0],confirmSplit[2])
+# confirmSplit[3] = coordinateCalculation(confirmSplit[1],confirmSplit[3])
+# pg.screenshot('./img/confirm.png', region=(confirmSplit[0], confirmSplit[1], confirmSplit[2], confirmSplit[3]))
 
 
 
